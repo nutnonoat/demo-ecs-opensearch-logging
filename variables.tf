d0551ea40e78955406f46f6c760d7d40d70940de
Oat <nuttanon@amazon.com>
2026-04-07 19:10:41 +0700

    chore: remove unused my_ip variable

--- variables.tf
+++ variables.tf
@@ -6,11 +6,6 @@ variable "project" {
   default = "demo-ecs-opensearch-logging"
 }
 
-variable "my_ip" {
-  description = "Your public IP for OpenSearch access (CIDR notation)"
-  type        = string
-}
-
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
